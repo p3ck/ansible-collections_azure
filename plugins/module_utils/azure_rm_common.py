aa0e408a5ae8f796433b6429d37834bae434255c
Fred-sun <37327967+Fred-sun@users.noreply.github.com>
2024-09-20 17:25:21 +0800

    Upgrade azure-mgmt-notificationhub api version to v2023-09-01 (#1723)

--- plugins/module_utils/azure_rm_common.py
+++ plugins/module_utils/azure_rm_common.py
@@ -1409,7 +1409,7 @@ class AzureRMModuleBase(object):
             self._notification_hub_client = self.get_mgmt_svc_client(
                 NotificationHubsManagementClient,
                 base_url=self._cloud_environment.endpoints.resource_manager,
-                api_version='2017-04-01')
+                api_version='2023-09-01')
         return self._notification_hub_client
 
     @property
